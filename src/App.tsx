609760c2a7d1a4d97dda52c552ca4c74718e13ee
Luciana Ramos <ramos.d.luciana@gmail.com>
2024-02-21 14:31:43 -0300

    cadastro de usuário

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import "./App.css";
-import NavBar from "./componentes/navBar/NavBar";
+
+import Navbar from "./componentes/navBar/NavBar";
 import Footer from "./componentes/footer/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Home from "./pages/home/Home";
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <NavBar/>
-    <div className="min-h-[80vh]">
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      </Routes>
-    </div>
-    <Footer/>
-    </BrowserRouter>
+   
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+  
     </>
- );
+  );
 }
 export default App;
